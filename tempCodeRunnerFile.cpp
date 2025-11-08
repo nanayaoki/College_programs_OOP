48dd2287036072d646f04adfa8574a8016acdbbb
Om-Adpawar <adpawarom08@gmail.com>
2025-11-08 12:20:23 +0530

    This pointer

---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+class age{
+    public:
+    int y;
+    age(int y){
+        this->y=y;
+    }
+
+    void display(){
+    cout << "Age is: " << y << " years" << endl;
+    }
+};
+
+int main(){
+
+    int years;
+    cout << "Enter age in years: ";
+    cin >> years;
+
+    age a1 = years;  // Implicit conversion from int to age
+    a1.display();
+
+    return 0;
+}
